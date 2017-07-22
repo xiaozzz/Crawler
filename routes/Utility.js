b7f952f2a85fe4ff736ecd402488878ffa1ee39f
zzzkky <philip.20050132@163.com>
2017-07-22 21:14:12 +0800

    update

--- routes/Utility.js
+++ routes/Utility.js
@@ -13,57 +13,79 @@ var newsSchema = new Schema({
     date : String,
     abstract : String,
     url : String,
+    text : String,
     _id : String
 });
 //模型
 var News = mongoose.model('News', newsSchema);
 
-//北京大学
-var c1 = async function(){
-    var pageUrls = [];
-    var pageNum = 2;
-    for (var i = 1; i <= pageNum; i++){
-        pageUrls.push('http://www.oir.pku.edu.cn/index.php?g=portal&m=list&a=index&id=17&p=' + i);
-    }
-    pageUrls.forEach(function(pageUrl){
-        superagent.get(pageUrl)
+//北京大学详细页
+var parseBody = function(url) {
+    return new Promise(function (resolve,reject) {
+        superagent.get(url)
             .end(function(err,page){
                 var $ = cheerio.load(page.text);
-                //console.log(page.text);
-                var quoteUrls = $('div.media-body');
-                for(var i = 0;i < quoteUrls.length; i++){
-                    //console.log(quoteUrls.eq(i).text());
-                    var title = quoteUrls.eq(i).children("h4").children("a").text();
-                    var date = quoteUrls.eq(i).children("ul").children("li").eq(0).text();
-                    var abstract = quoteUrls.eq(i).children("p").text();
-                    var url = "http://www.oir.pku.edu.cn" + quoteUrls.eq(i).children("h4").children("a").attr("href");
-
-                    var md5 = crypto.createHash('md5');
-                    var _id = md5.update(title+date+abstract+url).digest('base64');
-
-                    console.log(quoteUrls.eq(i).children("h4").children("a").text());
-                    console.log(quoteUrls.eq(i).children("ul").children("li").eq(0).text());
-                    console.log(quoteUrls.eq(i).children("p").text());
-                    console.log("http://www.oir.pku.edu.cn" + quoteUrls.eq(i).children("h4").children("a").attr("href"));
-                    console.log(_id);
-
-                    var news = new News({
-                        title : title,
-                        date : date,
-                        abstract : abstract,
-                        url : url,
-                        _id : _id
-                    });
-                    news.save(function(err) {
-                        if (err) {
-                            console.log('保存失败');
-                            return;
-                        }
-                        console.log('保存成功');
-                    })
-                }
-            })
+                var newsText = $('div.nr-news-text');
+                resolve(newsText.children('p').text());
+            });
     })
+};
+
+//北京大学新闻主页
+var c1 = async function(){
+    try {
+        //新闻页面URL及页数
+        var pageUrls = [];
+        var pageNum = 2;
+        for (var i = 1; i <= pageNum; i++) {
+            pageUrls.push('http://www.oir.pku.edu.cn/index.php?g=portal&m=list&a=index&id=17&p=' + i);
+        }
+        pageUrls.forEach(function (pageUrl) {
+            superagent.get(pageUrl)
+                .end(async function (err, page) {
+                    var $ = cheerio.load(page.text);
+                    //console.log(page.text);
+                    var quoteUrls = $('div.media-body');
+                    for (var i = 0; i < quoteUrls.length; i++) {
+                        //console.log(quoteUrls.eq(i).text());
+                        var title = quoteUrls.eq(i).children("h4").children("a").text();
+                        var date = quoteUrls.eq(i).children("ul").children("li").eq(0).text();
+                        var abstract = quoteUrls.eq(i).children("p").text();
+                        var url = "http://www.oir.pku.edu.cn" + quoteUrls.eq(i).children("h4").children("a").attr("href");
+
+                        var md5 = crypto.createHash('md5');
+                        var _id = md5.update(title + date + abstract + url).digest('base64');
+
+                        var text = await parseBody(url);
+
+                        // console.log(text);
+                        // console.log(quoteUrls.eq(i).children("h4").children("a").text());
+                        // console.log(quoteUrls.eq(i).children("ul").children("li").eq(0).text());
+                        // console.log(quoteUrls.eq(i).children("p").text());
+                        // console.log("http://www.oir.pku.edu.cn" + quoteUrls.eq(i).children("h4").children("a").attr("href"));
+                        // console.log(_id);
+
+                        var news = new News({
+                            title: title,
+                            date: date,
+                            abstract: abstract,
+                            url: url,
+                            text: text,
+                            _id: _id
+                        });
+                        news.save(function (err) {
+                            if (err) {
+                                console.log('保存失败');
+                                return;
+                            }
+                            console.log('保存成功');
+                        })
+                    }
+                })
+        })
+    } catch (err){
+        console.log(err);
+    }
 };
 
 
